40f9bdb22ccdcf7b35aa8c974e77a2497c7ec8d7
mitna_test <mitna034@gmail.com>
2019-09-09 02:44:07 +0900

    no message

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .get('/n/:status', (req, res) => {
+  .get('/result/:status', (req, res) => {
      var status = req.params.status
      res.json({result:status})
   })
